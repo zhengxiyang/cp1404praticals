8ebe9d2f77b9e06c4e45f6f0072c46a70eacbca2
zhengxiyang <zhengxi.yang@my.jcu.edu.au>
2023-12-17 20:44:37 +0800

    add prac06

---
+++ prac_06/used_cars.py
@@ -0,0 +1,16 @@
+from prac_06.car import Car
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180)
+    my_car.drive(30)
+    print(f"Car has fuel: {my_car.fuel}")
+    print(my_car)
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
+
+main()
