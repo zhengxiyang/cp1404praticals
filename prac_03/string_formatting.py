cc946fbd28791e7378d5ff93687f9a99a4d066af
zhengxiyang <zhengxi.yang@my.jcu.edu.au>
2024-10-06 21:19:05 +0800

    updated codes

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,11 +1,3 @@
-"""
-CP1404/CP5632 - Practical
-Various examples of using Python string formatting.
-(We prefer f-strings in this subject.)
-Want to read more about it?
-https://docs.python.org/3/library/string.html#formatstrings
-"""
-
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.4
